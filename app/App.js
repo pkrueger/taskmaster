adba378458306957ce8eddfdca113aa2d6ba5c66
pkrueger <sendthistopatrick@gmail.com>
2022-09-09 15:49:59 -0600

    Connect controllers to html

--- app/App.js
+++ app/App.js
@@ -1,3 +1,9 @@
-class App {}
+import { TaskListsController } from "./Controllers/TaskListsController.js";
+import { TasksController } from "./Controllers/TasksController.js";
+
+class App {
+  tasksController = new TasksController();
+  taskListsController = new TaskListsController();
+}
 
 window["app"] = new App();
